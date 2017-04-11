a67e22fd0f70cb14e7d26b366b6e2bda32265e99
Norzeteus <norbert.fridrich@chello.at>
2017-04-11 08:49:17 +0200

    Deleted boolean p_18....., now it works, correct?

--- src/main/java/com/space/extended/basicblocks/BlockMetalFrameCatway.java
+++ src/main/java/com/space/extended/basicblocks/BlockMetalFrameCatway.java
@@ -37,7 +37,7 @@ public class BlockMetalFrameCatway extends BlockMetalFrame {
 
 	@Override
 	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox,
-			List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean p_185477_7_) {
+			List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn_) {
 		int meta = state.getValue(FACING).getHorizontalIndex();
 
 		if (type != CatwayType.POST) {
